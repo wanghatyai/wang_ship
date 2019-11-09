de1110bf49f8324aeb10d57af06278a2c39d951d
wangAppPC <wanghatyai@gmail.com>
2019-11-09 16:32:45 +0700

    add plugin intl 0.16.0 for use dateformat get sum order update sum order UI

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:intl/intl.dart';
 
 import 'package:wang_ship/check_order.dart';
 import 'package:wang_ship/report.dart';
@@ -26,6 +27,14 @@ class _HomeState extends State<Home> {
   String usernameVal;
   var username;
 
+  var orderSum;
+  var orderSumFinish;
+
+  var orderBorderSum;
+  var orderBorderSumFinish;
+  
+  final formatDateTime = DateFormat('dd/MM/yyyy');
+
   int currentIndex = 0;
   List pages = [CheckOrderPage(), ReportPage(), ReportAllPage(), ReportBorderAllPage()];
 
@@ -37,11 +46,113 @@ class _HomeState extends State<Home> {
     return username;
   }
 
+  getOrderSum() async{
+
+    final res = await http.get('https://wangpharma.com/API/shippingProduct.php?act=GetShipAllSum');
+
+    if(res.statusCode == 200){
+
+      setState(() {
+
+        var jsonData = json.decode(res.body);
+
+        jsonData.forEach((orderSumDB) {
+          orderSum = orderSumDB['orderSum'];
+        });
+
+        print(orderSum);
+        return orderSum;
+
+      });
+
+    }else{
+      throw Exception('Failed load Json');
+    }
+  }
+
+  getOrderSumFinish() async{
+
+    final res = await http.get('https://wangpharma.com/API/shippingProduct.php?act=GetShipAllSumFinish');
+
+    if(res.statusCode == 200){
+
+      setState(() {
+
+        var jsonData = json.decode(res.body);
+
+        jsonData.forEach((orderSumDB) {
+          orderSumFinish = orderSumDB['orderSumFinish'];
+        });
+
+        print(orderSumFinish);
+        return orderSumFinish;
+
+      });
+
+    }else{
+      throw Exception('Failed load Json');
+    }
+  }
+
+  getOrderBorderSum() async{
+
+    final res = await http.get('https://wangpharma.com/API/shippingProduct.php?act=GetShipBorderAllSum');
+
+    if(res.statusCode == 200){
+
+      setState(() {
+
+        var jsonData = json.decode(res.body);
+
+        jsonData.forEach((orderSumDB) {
+          orderBorderSum = orderSumDB['orderBorderSum'];
+        });
+
+        print(orderBorderSum);
+        return orderBorderSum;
+
+      });
+
+    }else{
+      throw Exception('Failed load Json');
+    }
+  }
+
+  getOrderBorderSumFinish() async{
+
+    final res = await http.get('https://wangpharma.com/API/shippingProduct.php?act=GetShipBorderAllSumFinish');
+
+    if(res.statusCode == 200){
+
+      setState(() {
+
+        var jsonData = json.decode(res.body);
+
+        jsonData.forEach((orderSumDB) {
+          orderBorderSumFinish = orderSumDB['orderBorderSumFinish'];
+        });
+
+        print(orderBorderSumFinish);
+        return orderBorderSumFinish;
+
+      });
+
+    }else{
+      throw Exception('Failed load Json');
+    }
+  }
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     getCodeEmpShip();
+
+    getOrderSum();
+    getOrderSumFinish();
+
+    getOrderBorderSum();
+    getOrderBorderSumFinish();
   }
 
 
@@ -82,7 +193,25 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.deepOrange,
-        title: Text("ระบบส่งสินค้า - รหัสพนักงาน ${username}"),
+        title: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text("รหัสพนักงาน ${username}"),
+                Text("HY ส่งแล้ว $orderSumFinish/$orderSum บิล", style: TextStyle(fontSize: 17),),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text("${formatDateTime.format(DateTime.now())}"),
+                Text("ต่างจังหวัด ส่งแล้ว $orderBorderSumFinish/$orderBorderSum บิล", style: TextStyle(fontSize: 17)),
+              ],
+            ),
+
+          ],
+        ),
         actions: <Widget>[
 
         ],
